ed83f16de474d6d76d2274888c8e748bbedb1915
ahmaddyd <ahmdyulian.86@gmail.com>
2022-11-27 11:38:53 +0700

    Update Manifest

--- __manifest__.py
+++ __manifest__.py
@@ -1,23 +1,23 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "weddingorganizer",
+    'name': "Wedding Organizer",
 
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+        Custom Module Odoo 14""",
 
     'description': """
-        Long description of module's purpose
+        Untuk pemesanan kebutuhan Wedding
     """,
 
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
+    'author': "Ahmad Yulian Dinata",
+    'website': "https://www.linkedin.com/in/ahmaddyd/",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Uncategorized',
+    'category': 'Administration',
     'version': '0.1',
+    'application': True,
 
     # any module necessary for this one to work correctly
     'depends': ['base'],
