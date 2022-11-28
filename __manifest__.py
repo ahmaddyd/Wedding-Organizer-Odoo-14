5ebbdda41779270f2a89e7186e943a3ace208429
ahmaddyd <ahmdyulian.86@gmail.com>
2022-11-29 01:53:19 +0700

    Change category from manifest.py

--- __manifest__.py
+++ __manifest__.py
@@ -15,7 +15,7 @@
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Administration',
+    'category': 'Productivity',
     'version': '0.1',
     'application': True,
 
